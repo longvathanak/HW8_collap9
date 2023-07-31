d98720c4ab4c64227b9dcf71e7da897164bc1742
LONG Vathanak <2023229long@aupp.edu.kh>
2023-07-31 11:49:05 +0700

    .

--- exercise-3.py
+++ exercise-3.py
@@ -1,20 +1,2 @@
 def remove_all_after(numbers, n):
-    ...
-    # check if the numbers is empty
-    if not numbers: 
-        return []
-    
-    # Iterate through the numbers to find the border element
-    for index, element in enumerate(numbers):
-        if element == n: 
-            # return the numbers up to the indext where the border element is found
-            return numbers[:index+1]
-        
-    #if the border element is not found return the original numbers 
-    return numbers
-   
-#test case 
-print(remove_all_after([1,2,3,4,5], 2))
-print(remove_all_after([], 3))
-print(remove_all_after([2,2,3,4,1,6], ()))
 
